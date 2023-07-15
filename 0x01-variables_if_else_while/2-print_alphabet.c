2f8059157983e257abc3a20ecbe1323fec2a5229
Elha-ssan <elhassanouizan@gmail.com>
2023-07-14 17:11:15 -0700

    if less while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+/*  header goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++) {
+        putchar(letter);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
